2cb40ba7a46e84ed2eb8dfc7f803499cc136bb45
niels-van-den-broeck <niels.van.den.broeck@pelckmansuitgevers.be>
2020-09-26 01:52:41 +0200

    feat(app.ts): add v1 router to app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import express from 'express';
 
+import registerV1Routes from './api/routes/v1';
+
 export default function buildApp() {
   const app = express();
+  app.use(express.json());
+
+  const v1Router = express.Router();
+
+  app.use('/api/v1', v1Router);
+  registerV1Routes(v1Router);
 
   return app;
 }
