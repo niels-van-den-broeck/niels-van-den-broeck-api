1181c1301f7c9bf76d946b10736d9a36de12ac12
niels-van-den-broeck <niels.van.den.broeck@pelckmansuitgevers.be>
2020-09-26 15:06:05 +0200

    feat(register.post): add password validation to register api

--- src/api/routes/v1/user/register.post.test.ts
+++ src/api/routes/v1/user/register.post.test.ts
@@ -4,18 +4,23 @@ import buildApp from '../../../../app';
 
 type RegisterUserResouce = {
   email: string;
+  password: string;
 };
 
 describe('POST /api/v1/user/register', () => {
   const app = buildApp();
 
-  function createResource({ email = 'niels@test.be' }: Partial<RegisterUserResouce> = {}) {
+  function createResource({
+    email = 'niels@test.be',
+    password = 'verysecure001',
+  }: Partial<RegisterUserResouce> = {}): Partial<RegisterUserResouce> {
     return {
       email,
+      password,
     };
   }
 
-  function act(resource = {}) {
+  function act(resource = createResource()) {
     const req = request(app).post('/api/v1/user/register').send(resource);
 
     return req;
@@ -29,11 +34,48 @@ describe('POST /api/v1/user/register', () => {
 
   describe('HTTP 1.1/400 Bad Request', () => {
     test('it returns the status if user did not provide a valid email address', async () => {
-      await act().expect(400, {
+      const resource = createResource();
+      delete resource.email;
+
+      await act(resource).expect(400, {
         statusCode: 400,
         error: 'Bad Request',
         message: 'Request body is not valid',
         data: [{ key: 'email', message: '"email" is required' }],
+      });
+    });
+
+    test('it returns the status if user did not provide a valid password', async () => {
+      const resource = createResource();
+      delete resource.password;
+
+      await act(resource).expect(400, {
+        statusCode: 400,
+        error: 'Bad Request',
+        message: 'Request body is not valid',
+        data: [{ key: 'password', message: '"password" is required' }],
+      });
+    });
+
+    test('it returns the status if password does not contain a number', async () => {
+      await act(createResource({ password: 'notsecure' })).expect(400, {
+        statusCode: 400,
+        error: 'Bad Request',
+        message: 'Request body is not valid',
+        data: [
+          { key: 'password', message: 'Password must contain at least 6 characters and 1 number' },
+        ],
+      });
+    });
+
+    test('it returns the status if password is shorter than 6 characters', async () => {
+      await act(createResource({ password: 'nots1' })).expect(400, {
+        statusCode: 400,
+        error: 'Bad Request',
+        message: 'Request body is not valid',
+        data: [
+          { key: 'password', message: 'Password must contain at least 6 characters and 1 number' },
+        ],
       });
     });
   });
--- src/api/routes/v1/user/register.post.ts
+++ src/api/routes/v1/user/register.post.ts
@@ -6,6 +6,10 @@ import validateRequest from '../../../../middleware/requestValidator';
 
 const validationSchema = Joi.object({
   email: Joi.string().email().required(),
+  password: Joi.string()
+    .pattern(/^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$/)
+    .message('Password must contain at least 6 characters and 1 number')
+    .required(),
 });
 
 const registerRoute: RouteRegistration = (router) => {
